db1e597341560e4b151aafefdd5c5758e4e1a7b2
cauanwarlly <148079161+cauanwarlly@users.noreply.github.com>
2025-04-11 16:41:56 -0300

    atualizaçao
    dia 11/04/2025

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from src.load import salvarCsv, salvarSqlite, salavarMySQL
 dadosBcb = requestApiBCB("20191")
 
 # SALVA O BANCO DE DADOS EM CSV
-# salvarCsv(dadosBcb, "src/datasets/MeiosdePagamentosTri.csv",",",".")
+salvarCsv(dadosBcb, "src/datasets/MeiosdePagamentosTri.csv",",",".")
 
 # SALVA O BANCO DE DADOS EM SQL
-# salvarSqlite (dadosBcb, "src/datasets/etlbcb.db", "meios_pagamentos_tri")
+salvarSqlite (dadosBcb, "src/datasets/etlbcb.db", "meios_pagamentos_tri")
 
 # salva e jogando no banco MySQL
 salavarMySQL(dadosBcb, "root", "root", "localhost", "etlbcb", "meios_pagamentos_tri")
